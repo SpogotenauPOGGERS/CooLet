7bc2f51f9e870d2350e1c0674876aeb59e44260a
J.Vontobel.inf21 <j.vontobel.inf21@stud.bbbaden.ch>
2023-05-18 23:28:24 +0200

    added quiz creation

--- addQuiz.js
+++ addQuiz.js
@@ -37,7 +37,7 @@ export default function renderAddQuizPage() {
   if (getToken() == null) {
     noUserModal.showModal()
   }
-  closeModalButton.addEventListener('click', (re) => {
+  closeModalButton.addEventListener('click', (e) => {
     noUserModal.close()
     window.location.hash = '#login'
   })
@@ -69,7 +69,82 @@ function createTitle(e) {
     body: JSON.stringify(QuizData),
   })
     .then((res) => res.json())
-    .then((data) => console.log(data))
+    .then((data) => {
+      console.log(data)
+      clearElement(mainBody)
+      const questionsTitle = document.createElement('h1')
+      questionsTitle.classList.add('questionsTitle')
+      questionsTitle.textContent = 'Here you can add your terms'
+      const questionsForm = document.createElement('form')
+      questionsForm.classList.add('questionsForm')
+      const language1Input = document.createElement('input')
+      language1Input.classList.add('languageInput')
+      language1Input.required = true
+      language1Input.placeholder = 'Write your Term in here'
+      const language2Input = document.createElement('input')
+      language2Input.classList.add('languageInput')
+      language2Input.required = true
+      language2Input.placeholder = 'And here the translation'
+      const submitQuestionsButton = document.createElement('button')
+      submitQuestionsButton.classList.add('submitQuestionButton')
+      submitQuestionsButton.textContent = 'Add these Question to the quiz'
+      submitQuestionsButton.type = 'submit'
+      const addAnotherTermButton = document.createElement('button')
+      addAnotherTermButton.classList.add('addAnotherTermButton')
+      addAnotherTermButton.innerHTML = `<i class="fa-solid fa-plus"></i>`
+
+      let quizQuestions = []
+      questionsForm.addEventListener('submit', (e) => {
+        e.preventDefault()
+        let question = {
+          language1: language1Input.value,
+          language2: language2Input.value,
+        }
+        const termAndTranslation = document.createElement('div')
+        termAndTranslation.classList.add('termAndTranslation')
+        const language1 = document.createElement('div')
+        language1.classList.add('language')
+        language1.textContent = language1Input.value
+        const language2 = document.createElement('div')
+        language2.classList.add('language')
+        language2.textContent = language2Input.value
+        termAndTranslation.append(language1)
+        termAndTranslation.append(language2)
+        mainBody.append(termAndTranslation)
+        quizQuestions.push(question)
+        language1Input.value = ''
+        language2Input.value = ''
+      })
+
+      addAnotherTermButton.addEventListener('click', (e) => {
+        e.preventDefault()
+        let question = {
+          language1: language1Input.value,
+          language2: language2Input.value,
+        }
+        const termAndTranslation = document.createElement('div')
+        termAndTranslation.classList.add('termAndTranslation')
+        const language1 = document.createElement('div')
+        language1.classList.add('language')
+        language1.textContent = language1Input.value
+        const language2 = document.createElement('div')
+        language2.classList.add('language')
+        language2.textContent = language2Input.value
+        termAndTranslation.append(language1)
+        termAndTranslation.append(language2)
+        mainBody.append(termAndTranslation)
+        quizQuestions.push(question)
+        language1Input.value = ''
+        language2Input.value = ''
+      })
+
+      mainBody.append(questionsTitle)
+      questionsForm.append(language1Input)
+      questionsForm.append(language2Input)
+      questionsForm.append(submitQuestionsButton)
+      questionsForm.append(addAnotherTermButton)
+      mainBody.append(questionsForm)
+    })
 }
 
 function clearElement(element) {
