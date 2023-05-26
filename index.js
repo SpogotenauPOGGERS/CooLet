22391457b6648193484dc22d6c6d7633148e2cbe
N.Manojlovic.inf21 <n.manojlovic.inf21@stud.bbbaden.ch>
2023-05-26 11:20:48 +0200

    learnMode

--- index.js
+++ index.js
@@ -1,27 +1,34 @@
-const recoms = document.querySelector('.recommendations')
-const searchButton = document.querySelector('.searchButton')
-const searchedSet = document.querySelector('.searchedSet')
-const placeForSearchedSets = document.querySelector('.searchedSets')
-const mainBody = document.querySelector('.main')
+const recoms = document.querySelector(".recommendations")
+const searchButton = document.querySelector(".searchButton")
+const searchedSet = document.querySelector(".searchedSet")
+const placeForSearchedSets = document.querySelector(".searchedSets")
+const mainBody = document.querySelector(".main")
+const questionCard = document.createElement("div")
+questionCard.classList.add("questionCard")
+const frontSide = document.createElement("div")
+frontSide.classList.add("frontSide")
+const backSide = document.createElement("div")
+backSide.classList.add("backSide")
+let i = 0
 
 export default function renderHomePage() {
   clearElement(mainBody)
   searchedSet.disabled = false
-  const welcome = document.createElement('h1')
-  welcome.textContent = 'Welcome to CooLet'
-  welcome.classList.add('welcome')
+  const welcome = document.createElement("h1")
+  welcome.textContent = "Welcome to CooLet"
+  welcome.classList.add("welcome")
   mainBody.append(welcome)
-  const placeForSearchedSets = document.createElement('div')
-  placeForSearchedSets.classList.add('searchedSets')
+  const placeForSearchedSets = document.createElement("div")
+  placeForSearchedSets.classList.add("searchedSets")
   mainBody.append(placeForSearchedSets)
-  const recomTitle = document.createElement('h1')
-  recomTitle.textContent = 'Here are some recommendations'
-  recomTitle.classList.add('recommTitle')
+  const recomTitle = document.createElement("h1")
+  recomTitle.textContent = "Here are some recommendations"
+  recomTitle.classList.add("recommTitle")
   mainBody.append(recomTitle)
-  const recoms = document.createElement('div')
-  recoms.classList.add('recommendations')
+  const recoms = document.createElement("div")
+  recoms.classList.add("recommendations")
   mainBody.append(recoms)
-  fetch('http://localhost:9000/')
+  fetch("http://localhost:9000/")
     .then((res) => res.json())
     .then((data) => {
       console.log(data.length)
@@ -32,22 +39,22 @@ export default function renderHomePage() {
 
         if (!randomNumbers.includes(randomNumber)) {
           randomNumbers.push(randomNumber)
-          const quizzesDiv = document.createElement('div')
+          const quizzesDiv = document.createElement("div")
           quizzesDiv.innerHTML = `
       <h1>${data[randomNumber].setName}</h1>
       <p>${data[randomNumber].creator}</p>
       `
-          quizzesDiv.addEventListener('click', (e) =>
+          quizzesDiv.addEventListener("click", (e) =>
             renderLearnMode(data[randomNumber])
           )
 
-          quizzesDiv.classList.add('quizzes')
+          quizzesDiv.classList.add("quizzes")
           recoms.append(quizzesDiv)
         }
       }
     })
 
-  searchButton.addEventListener('click', (e) => {
+  searchButton.addEventListener("click", (e) => {
     e.preventDefault()
     const searchedSetValue = searchedSet.value
     console.log(searchedSetValue)
@@ -57,21 +64,21 @@ export default function renderHomePage() {
       .then((data) => {
         console.log(data)
         if (data.length === 0) {
-          const nothingFoundText = document.createElement('h2')
-          nothingFoundText.textContent = 'There is no set like this'
-          nothingFoundText.classList.add('nothingFound')
+          const nothingFoundText = document.createElement("h2")
+          nothingFoundText.textContent = "There is no set like this"
+          nothingFoundText.classList.add("nothingFound")
           placeForSearchedSets.append(nothingFoundText)
         } else {
           data.forEach((quizzes) => {
-            const quizzesDiv = document.createElement('div')
+            const quizzesDiv = document.createElement("div")
             quizzesDiv.innerHTML = `
           <h1>${quizzes.setName}</h1>
           <p>${quizzes.creator}</p>`
-            quizzesDiv.addEventListener('click', (e) =>
+            quizzesDiv.addEventListener("click", (e) =>
               renderLearnMode(quizzes)
             )
 
-            quizzesDiv.classList.add('searchedQuizzes')
+            quizzesDiv.classList.add("searchedQuizzes")
             placeForSearchedSets.append(quizzesDiv)
           })
         }
@@ -89,5 +96,59 @@ function renderLearnMode(quiz) {
   console.log(quiz)
   clearElement(mainBody)
 
-  console.log(quiz.questions[3].language)
+  const prevQuestion = document.createElement("button")
+  prevQuestion.classList.add("prevQuestion")
+  prevQuestion.innerHTML = `<i class="fa-solid fa-arrow-left"></i>`
+  const nextQuestion = document.createElement("button")
+  nextQuestion.classList.add("nextQuestion")
+  nextQuestion.innerHTML = `<i class="fa-solid fa-arrow-right"></i>`
+  const buttonPlace = document.createElement("div")
+  buttonPlace.classList.add("buttonPlace")
+  buttonPlace.append(prevQuestion, nextQuestion)
+
+  nextQuestion.addEventListener("click", function () {
+    showNextQuestion(quiz, frontSide, backSide)
+  })
+
+  prevQuestion.addEventListener("click", function () {
+    showPrevQuestion(quiz, frontSide, backSide)
+  })
+
+  frontSide.textContent = quiz.questions[i].language1
+  backSide.textContent = quiz.questions[i].language2
+
+  questionCard.append(frontSide)
+  questionCard.append(backSide)
+  mainBody.append(questionCard)
+  mainBody.append(buttonPlace)
+}
+
+function showNextQuestion(quiz, frontSide, backSide) {
+  console.log(i)
+  let length = quiz.questions.length - 1
+
+  console.log(length)
+
+  if (i < length) {
+    i++
+    frontSide.textContent = quiz.questions[i].language1
+    backSide.textContent = quiz.questions[i].language2
+    return i
+  } else {
+    console.log("Hallo")
+    return i
+  }
+}
+
+function showPrevQuestion(quiz, frontSide, backSide) {
+  console.log(i)
+  if (i > 0) {
+    i--
+    frontSide.textContent = quiz.questions[i].language1
+    backSide.textContent = quiz.questions[i].language2
+    return i
+  } else {
+    console.log("Hallo")
+    return i
+  }
 }
--- routing/routes.js
+++ routing/routes.js
@@ -1,17 +1,17 @@
-import Router from './Router.js'
-import renderHomePage from '../index.js'
-import renderLogin from '../login.js'
-import renderRegistrationPage from '../registration.js'
-import renderProfilePage from '../profilePage.js'
-import renderAddQuizPage from '../addQuiz.js'
+import Router from "./Router.js"
+import renderHomePage from "../index.js"
+import renderLogin from "../login.js"
+import renderRegistrationPage from "../registration.js"
+import renderProfilePage from "../profilePage.js"
+import renderAddQuizPage from "../addQuiz.js"
 
 const routes = {
-  homePage: { hash: '#home', function: renderHomePage },
-  createSet: { hash: '#createSet' },
-  login: { hash: '#login', function: renderLogin },
-  register: { hash: '#register', function: renderRegistrationPage },
-  profile: { hash: '#profile', function: renderProfilePage },
-  addQuiz: { hash: '#createQuiz', function: renderAddQuizPage },
+  homePage: { hash: "#home", function: renderHomePage },
+  createSet: { hash: "#createSet" },
+  login: { hash: "#login", function: renderLogin },
+  register: { hash: "#register", function: renderRegistrationPage },
+  profile: { hash: "#profile", function: renderProfilePage },
+  addQuiz: { hash: "#createQuiz", function: renderAddQuizPage },
 }
 
 const router = new Router(routes)
